8cdc3550ab2bdb3a0e24171bbc3348a648862b4c
Ghost <taylerobin@gmail.com>
2024-01-14 06:11:38 -0700

    More comments and better console output.

--- Steam_No_Browser.cmd
+++ Steam_No_Browser.cmd
@@ -6,50 +6,73 @@ setlocal EnableDelayedExpansion
 set "steam_path=%SystemDrive%\Program Files (x86)\Steam"
 :: Game to launch
 set app_id=368260
-:: Time in seconds to wait for steam to start
+:: Time in seconds to wait for Steam to start
 set init_time=20
 
-:: Check if steam.exe exists in path, if not print error.
+:: Check if Steam.exe exists in path, if not print error.
 if not exist "!steam_path!\steam.exe" (
-    echo =======ERROR=============================
-    echo "Steam.exe" not found. Please ensure
-    echo the 'steam_path' variable in the script
-    echo matches your steam installation folder.
-    echo =========================================
+    echo.
+    echo    ษอออออออออออออออออออออออออออออออออออออออออป
+    echo    บ  "Steam.exe" not found. Please ensure   บ
+    echo    บ the 'steam_path' variable in the script บ
+    echo    บ matches your steam installation folder. บ
+    echo    ศอออออออออออออออออออออออออออออออออออออออออผ
     echo.
     pause
-    exit
+    exit /b 1
 )
 
-:: Kill steam if it's running
-tasklist | findstr /i "steam.exe" && taskkill /f /im "steam.exe"
+echo Force closing Steam if running, and ensuring steamwebhelper is available for launch.
+:: Kill Steam if it's running
+taskkill /f /im "steam.exe" 2>nul
 
-:: Restore binary back as steam requires it to initialize.
-call :BinaryDance "webhelper" "steamwebhelper.exe"
+:: Restore binary as steam requires it to initialize.
+call :BinaryDance "hiddenwebhelper" "steamwebhelper.exe"
 
+echo.
+echo Launching Steam and App #!app_id!.
 :: Open steam and specified app
 start "" "!steam_path!\steam.exe" steam://rungameid/!app_id! --disable-background-networking --enable-low-end-device-mode --single-process -cef-single-process -skipinitialbootstrap -quicklogin -oldtraymenu -silent
 
+echo.
+echo Waiting to make sure Steam has launched before killing steamwebhelper
+echo (Note, if this timing doesn't work for you, just modify init_time in the script.)
 :: Only continue if steamwebhelper is running in case steam is updating
 :wait_for_steamwebhelper
-tasklist | findstr /i "steamwebhelper.exe" || goto :wait_for_steamwebhelper
+tasklist | findstr /i "steamwebhelper.exe" >nul 2>&1|| goto :wait_for_steamwebhelper
 
 :: Allow a few seconds for steam to initialize/login
 timeout /t !init_time! /nobreak
 
+echo.
+echo Killing webhelper and renaming the exe.
 :: Hide binary and kill the process
-call :BinaryDance "steamwebhelper.exe" "webhelper" "kill"
+call :BinaryDance "steamwebhelper.exe" "hiddenwebhelper" "kill"
+
+:: Prints a message instructing the user to keep the window open and close Steam when done.
+echo.
+echo.
+echo       ษอออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออป
+echo       บ   Leave this window open and when done close Steam either   บ
+echo       บ by right clicking it on the taskbar, or in the task manager บ
+echo       บ    This ensures things are put back how they should be.     บ
+echo       ศอออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออผ
+echo.
+echo.
 
 :: Wait for steam to be closed and then rename webhelper back.
 :wait_to_fix
-timeout /t 10 /nobreak  
-tasklist | find /i "steam.exe" >nul 2>&1
+ping -n 11 localhost >nul
+tasklist /fi "imagename eq steam.exe" 2>nul | find /i "steam.exe">nul 2>&1
 IF ERRORLEVEL 1 (
-    call :BinaryDance "webhelper" "steamwebhelper.exe"
+    call :BinaryDance "hiddenwebhelper" "steamwebhelper.exe"
+    echo Program complete. You may close the window.
 ) ELSE (
     goto wait_to_fix
 )
-exit/b
+
+pause
+exit/b 0
 
 :: Used for renaming steamwebhelper back and forth. 
 :: Delete the source if the destination already exists. 
@@ -65,16 +88,11 @@ for %%a in ("cef.win7", "cef.win7x64") do (
     if exist "!src!" (
         if "%~3"=="kill" (
             taskkill /f /im "%~1"
-            echo %~1 killed
         )
         if exist "!dest!" (
-            del "!src!"
-            echo %~1 deleted as %~2 already exists
-            echo.
+            del "!src!" >nul 2>&1 && echo %~1 deleted as %~2 already exists
         ) else (
-            move "!src!" "!dest!"
-            echo %~1 moved to %~2
-            echo.
+            move "!src!" "!dest!" >nul && echo %~1 renamed to %~2
         )
     )
 )
