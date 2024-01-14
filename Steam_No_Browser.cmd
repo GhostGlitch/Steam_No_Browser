1b7a8d548c14647919f7ec1140ff337f5b1993e2
Ghost <taylerobin@gmail.com>
2024-01-14 14:40:18 -0700

    Improve error handling,, add timeout for webhelper start-up, and move Boxed Messages out of the way.

--- Steam_No_Browser.cmd
+++ Steam_No_Browser.cmd
@@ -5,19 +5,15 @@ setlocal EnableDelayedExpansion
 :: Steam directory
 set "steam_path=%SystemDrive%\Program Files (x86)\Steam"
 :: Game to launch
-set app_id=368260
+set "app_id=368260"
 :: Time in seconds to wait for Steam to start
-set init_time=20
+set "init_time=20"
+:: Time in seconds before giving up on webhelper starting
+set /a "max_wait_for_update=90"
 
 :: Check if Steam.exe exists in path, if not print error.
 if not exist "!steam_path!\steam.exe" (
-    echo.
-    echo    ษอออออออออออออออออออออออออออออออออออออออออป
-    echo    บ  "Steam.exe" not found. Please ensure   บ
-    echo    บ the 'steam_path' variable in the script บ
-    echo    บ matches your steam installation folder. บ
-    echo    ศอออออออออออออออออออออออออออออออออออออออออผ
-    echo.
+    call :PrntBox PathError
     pause
     exit /b 1
 )
@@ -38,9 +34,21 @@ echo.
 echo Waiting to make sure Steam has launched before killing steamwebhelper
 echo (Note, if this timing doesn't work for you, just modify init_time in the script.)
 :: Only continue if steamwebhelper is running in case steam is updating
+set /a "count=0"
 :wait_for_steamwebhelper
-tasklist | findstr /i "steamwebhelper.exe" >nul 2>&1|| goto :wait_for_steamwebhelper
+tasklist | findstr /i "steamwebhelper.exe" >nul 2>&1 && goto :steamwebhelper_started
+set /a "count+=1"
+if !count! lss !max_wait_for_update! (
+    :: Using ping as a sleep because it has more accurate timing, and a countdown is not needed.
+    ping -n 2 localhost >nul
+    goto :wait_for_steamwebhelper
+)
+call :PrntBox TimeoutError
+pause
+exit /b 1
 
+
+:steamwebhelper_started
 :: Allow a few seconds for steam to initialize/login
 timeout /t !init_time! /nobreak
 
@@ -50,18 +58,11 @@ echo Killing webhelper and renaming the exe.
 call :BinaryDance "steamwebhelper.exe" "hiddenwebhelper" "kill"
 
 :: Prints a message instructing the user to keep the window open and close Steam when done.
-echo.
-echo.
-echo       ษอออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออป
-echo       บ   Leave this window open and when done close Steam either   บ
-echo       บ by right clicking it on the taskbar, or in the task manager บ
-echo       บ    This ensures things are put back how they should be.     บ
-echo       ศอออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออผ
-echo.
-echo.
+call :PrntBox WaitInstruction
 
 :: Wait for steam to be closed and then rename webhelper back.
 :wait_to_fix
+:: Using ping as a sleep because it has more accurate timing, and a countdown is not needed.
 ping -n 11 localhost >nul
 tasklist /fi "imagename eq steam.exe" 2>nul | find /i "steam.exe">nul 2>&1
 IF ERRORLEVEL 1 (
@@ -70,7 +71,6 @@ IF ERRORLEVEL 1 (
 ) ELSE (
     goto wait_to_fix
 )
-
 pause
 exit/b 0
 
@@ -88,6 +88,10 @@ for %%a in ("cef.win7", "cef.win7x64") do (
     if exist "!src!" (
         if "%~3"=="kill" (
             taskkill /f /im "%~1"
+            if errorlevel 1 (
+                pause
+                exit /b 1
+                )
         )
         if exist "!dest!" (
             del "!src!" >nul 2>&1 && echo %~1 deleted as %~2 already exists
@@ -97,3 +101,41 @@ for %%a in ("cef.win7", "cef.win7x64") do (
     )
 )
 exit/b
+
+:: psuedo switch to print formatted message boxes for errors and instructions.
+:PrntBox
+echo.
+goto :%1 
+echo Error: Bad paramater passed to PrntBox. I should make a better error message for this...
+pause
+exit /b
+    :PathError
+    echo.
+    echo    ษอออออออออออออออออออออออออออออออออออออออออป
+    echo    บ  "Steam.exe" not found. Please ensure   บ
+    echo    บ the 'steam_path' variable in the script บ
+    echo    บ matches your steam installation folder. บ
+    echo    ศอออออออออออออออออออออออออออออออออออออออออผ
+    echo.
+    exit /b   
+    :TimeoutError
+    echo.
+    echo    ษออออออออออออออออออออออออออออออออออออออออออออออออออป
+    echo    บ Steamwebhelper has not started for !max_wait_for_update! seconds.   บ
+    echo    บ either Steam has not launched or it is updating. บ
+    echo    บ If !max_wait_for_update! seconds is not long enough, you can modify บ
+    echo    บ the 'max_wait_for_update' variable in the script บ
+    echo    ศออออออออออออออออออออออออออออออออออออออออออออออออออผ
+    echo.
+    exit /b
+    :WaitInstruction
+    echo.
+    echo.
+    echo       ษอออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออป
+    echo       บ   Leave this window open and when done close Steam either   บ
+    echo       บ by right clicking it on the taskbar, or in the task manager บ
+    echo       บ    This ensures things are put back how they should be.     บ
+    echo       ศอออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออผ
+    echo.
+    echo.
+    exit /b  
--- testing/tst.bat
+++ testing/tst.bat
@@ -1,7 +1,74 @@
-@echo off 
+REM THIS IS A TEST FILE. NOTHING IN HERE IS IMPORTANT (yet).
+REM I was just too lazy to set up .gitignore or branches.
+
+REM The file is currently being used to find a 
+REM way to generate PrintBoxes on the fly.
+
+@echo off
 setlocal EnableDelayedExpansion
-tasklist | findstr /i "steam.exe" >nul && taskkill /f /im >nul"steam.exe" 
 
-echo Terminating Steam...
+SET "mystr=Leave this window open. When done close Steam either by right clicking it on the taskbar, or in the task manager. This ensures things are put back how they should be."
+REM SET mynumber=120
+REM FOR /l %%i IN (2,1,%mynumber%) DO set "mystr=!mystr!%mystr%"
 
+REM ECHO +%mystr%+
+call :strlen length mystr
+call :funk result length 50
+echo !result!
 pause
+
+REM ********* function *****************************
+:strlen <resultVar> <stringVar>
+(   
+   set "tmp=!%~2!"
+   if defined tmp (
+      set "len=1"
+      for %%P in (4096 2048 1024 512 256 128 64 32 16 8 4 2 1) do (
+         if "!tmp:~%%P,1!" NEQ "" ( 
+            set /a "len+=%%P"
+            set "tmp=!tmp:~%%P!"
+         )
+      )
+   ) ELSE (
+      set "len=0"
+   )
+)
+endlocal
+set "%~1=%len%"
+exit /b
+
+:funk <resultVar> <lengthVar> <Max>
+set "len=!%~2!" 
+REM add zeroes
+echo len=%len%
+for /L %%d in (1,1,100) do (
+   echo %len%/%%~d=!tempres!
+   set /a "tmpres=%len%/%%~d"
+   echo tmpres=!tmpres!
+   if !tmpres! LEQ %~3 (
+      REM set /a integerPart=!tmp! /100
+      REM set /a decimalPart=!tmp! %% 100
+      REM set "%~1=!integerPart!.!decimalPart!"
+      set /a "%~1=!tmpres!"
+      exit /b
+   )
+)
+
+:Horrizontal
+SET "mychar=Í"
+FOR /l %%i IN (2,1,%~1) DO set "mychar=!mychar!%mychar%"
+IF %~3==t echo. && echo %~2É%mychar%»
+IF %~3==b echo %~2È%mychar%¼ && echo.
+exit /b
+
+:PrntBox
+goto :WaitInstruction
+exit /b
+    :WaitInstruction
+    echo.
+    call :Horrizontal 61 "      " t
+    echo       º   Leave this window open and when done close Steam either   º
+    echo       º by right clicking it on the taskbar, or in the task manager º
+    echo       º    This ensures things are put back how they should be.     º
+    call :Horrizontal 61 "      " b
+    echo.
